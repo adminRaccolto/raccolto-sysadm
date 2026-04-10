8b02763771ccf649ab744ee710c53ed78965e142
Gino M Migotto <gino@raccolto.com.br>
2026-04-10 13:46:54 -0400

    fix: import StorageModule into EmpresasModule for logo upload

--- backend/src/empresas/empresas.module.ts
+++ backend/src/empresas/empresas.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PerfisAcessoModule } from '../perfis-acesso/perfis-acesso.module';
+import { StorageModule } from '../storage/storage.module';
 import { EmpresasController } from './empresas.controller';
 import { EmpresasService } from './empresas.service';
 
 @Module({
-  imports: [PerfisAcessoModule],
+  imports: [PerfisAcessoModule, StorageModule],
   controllers: [EmpresasController],
   providers: [EmpresasService],
   exports: [EmpresasService],
